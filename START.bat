54ce957011b7ad45be28effa593c9d268e16e627
no1 <spielespiele403@gmail.com>
2026-04-22 17:19:18 +0200

    Add files via upload

---
+++ START.bat
@@ -0,0 +1,4 @@
+@echo off
+python -m pip install python-dotenv obsws-python
+python bot.py
+pause
---
+++ bot.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+import os
+import socket
+import json
+from dotenv import load_dotenv
+from obsws_python import ReqClient
+
+load_dotenv()
+
+with open('config.json') as f:
+    config = json.load(f)
+
+USERNAME = config['chat']['username']
+OAUTH = os.getenv('TWITCH_BOT_OAUTH')
+OBS_HOST = config['software']['host']
+OBS_PORT = config['software']['port']
+OBS_PASS = config['software']['password']
+
+print('=' * 50)
+print('OBS RECORDING BOT')
+print('=' * 50)
+print(f'Twitch: {USERNAME}')
+print(f'OBS: {OBS_HOST}:{OBS_PORT}')
+print('=' * 50)
+print()
+
+class OBS:
+    def __init__(self, host, port, pwd):
+        self.client = None
+        self.ok = False
+        try:
+            self.client = ReqClient(host=host, port=port, password=pwd)
+            self.ok = True
+            print('[OBS] Connected')
+        except Exception as e:
+            print(f'[OBS] Failed: {e}')
+    
+    def start(self):
+        if not self.ok:
+            return False
+        try:
+            self.client.start_record()
+            print('[REC] START')
+            return True
+        except Exception as e:
+            print(f'[REC] Error: {e}')
+            return False
+    
+    def stop(self):
+        if not self.ok:
+            return False
+        try:
+            self.client.stop_record()
+            print('[REC] STOP')
+            return True
+        except Exception as e:
+            print(f'[REC] Error: {e}')
+            return False
+
+class IRC:
+    def __init__(self, user, token, channel):
+        self.user = user
+        self.token = token
+        self.ch = channel
+        self.sock = None
+    
+    def connect(self):
+        try:
+            self.sock = socket.socket()
+            self.sock.connect(('irc.chat.twitch.tv', 6667))
+            self.send(f'PASS oauth:{self.token}')
+            self.send(f'NICK {self.user}')
+            self.send(f'JOIN #{self.ch}')
+            print('[IRC] Connected')
+            return True
+        except:
+            print('[IRC] Failed')
+            return False
+    
+    def send(self, msg):
+        try:
+            self.sock.send(f'{msg}\r\n'.encode())
+        except:
+            pass
+    
+    def say(self, msg):
+        self.send(f'PRIVMSG #{self.ch} :{msg}')
+    
+    def loop(self, obs):
+        try:
+            while True:
+                data = self.sock.recv(1024).decode('utf-8', errors='ignore')
+                for line in data.split('\r\n'):
+                    if 'PING' in line:
+                        self.send('PONG :tmi.twitch.tv')
+                    if 'PRIVMSG' in line and '!' in line:
+                        try:
+                            user = line.split('!')[0][1:]
+                            msg = line.split(':', 2)[2].lower().strip()
+                            print(f'[CMD] {user}: {msg}')
+                            if msg == '!rec':
+                                obs.start()
+                                self.say('Recording started')
+                            elif msg == '!stoprec':
+                                obs.stop()
+                                self.say('Recording stopped')
+                        except:
+                            pass
+        except KeyboardInterrupt:
+            print('[BOT] Stopped')
+
+obs = OBS(OBS_HOST, OBS_PORT, OBS_PASS)
+irc = IRC(USERNAME, OAUTH, USERNAME)
+irc.connect()
+
+print('[BOT] Ready\n')
+irc.loop(obs)
